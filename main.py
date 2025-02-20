9d8317a0adb44041c5ebc77286fb4b0da85bfbb4
mberriosmoreno <94083004+mberriosmoreno@users.noreply.github.com>
2025-02-19 21:45:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def about_me():
             st.image("assets/0. logo.png", caption="Demo interactiva")
 
     # Añadir los objetivos principales
+   st.title("📄 Detalles del Proyecto")
     with st.expander("📌 Objetivos principales", expanded=True):
         st.markdown("""
         - Crear una plataforma innovadora para...
